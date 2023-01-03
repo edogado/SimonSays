a7a6ea3d5d5cba3f23761f431a85ba7d057416f3
edomorales <edmorales94@gmail.com>
2023-01-03 18:36:21 -0500

    every time a button is pressed, its number representing its color is added to the pattern list, so it can be compared with the computer's pattern

--- simon.js
+++ simon.js
@@ -20,7 +20,7 @@ class Simon{
 
     async runGame(){
         this.createPattern();
-        await this.sleep(1000).then(()=> {})
+        await this.sleep(1000);
         for (let i =0; i < this.#pattern.length; i++){
             if (this.#pattern[i] === 1){
                 greenButton.style.background = 'limegreen';
@@ -59,20 +59,29 @@ class User{
             greenButton.style.background = 'limegreen';
             await this.sleep(1000);
             greenButton.style.background = 'darkgreen';
+            this.pattern.push(1);
         });
 
         redButton.addEventListener('click', async () => {
             redButton.style.background = 'red';
             await  this.sleep(1000);
             redButton.style.background = 'darkred';
+            this.pattern.push(2);
         });
 
         yellowButton.addEventListener('click', async ()=> {
             yellowButton.style.background = 'yellow';
             await this.sleep(1000);
             yellowButton.style.background = '#B58B00';
+            this.pattern.push(3);
         });
 
+        blueButton.addEventListener('click', async ()=> {
+            blueButton.style.background = 'blue';
+            await this.sleep(1000);
+            blueButton.style.background = 'darkblue';
+            this.pattern.push(4);
+        });
     }
 }
 
